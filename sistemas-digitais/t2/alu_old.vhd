4317655b7c811a050bcf50751965dcb314542e91
Natanael <natanael_magalhaes@msn.com>
2021-04-07 01:38:56 -0300

    entity

---
+++ sistemas-digitais/t2/alu_old.vhd
@@ -0,0 +1,22 @@
+library ieee;
+-- use ieee.numeric_std.all;
+use ieee.numeric_bit.all;
+
+-- Tentativas:
+-- 14110 (falha)
+
+
+entity alu is
+  generic(
+    size: natural := 10 -- bit size
+  );
+
+  port(
+    A, B: in bit_vector(size-1 downto 0); --inputs
+    F: out bit_vector(size-1 downto 0); -- outputs
+    S: in bit_vector(3 downto 0); -- op selection
+    Z: out bit; -- zero flag
+    Ov: out bit; -- overflow flag
+    Co: out bit -- carry out
+  );
+end entity alu;
