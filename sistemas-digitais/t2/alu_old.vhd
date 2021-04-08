11631b0908a55a855f3a10eaf9a26ca76eb8d9e7
Natanael <natanael_magalhaes@msn.com>
2021-04-08 01:38:56 -0300

    mux

--- sistemas-digitais/t2/alu_old.vhd
+++ sistemas-digitais/t2/alu_old.vhd
@@ -26,5 +26,28 @@ architecture alu_arch of alu is
   signal a_signed, b_signed, f_signed: signed(size downto 0) := (others => '0');
   signal f_expanded: bit_vector(size downto 0) := (others => '0');
   -- signal result: signed(size downto 0);
+begin
+  a_signed <= resize(signed(A), size + 1);
+  b_signed <= resize(signed(B), size + 1);
+
+  process(A, B, S)
+    variable result: signed(size downto 0);
+    -- variable f_expanded: bit_vector(size downto 0) := (others => '0');
+  begin
+    case S is
+      when "0000" =>
+        result := a_signed and b_signed;
+      when "0001" =>
+        result := a_signed or b_signed;
+      when "0010" =>
+        result := a_signed + b_signed;
+      when "0110" =>
+        result := a_signed - b_signed;
+      when "0111" =>
+        result := a_signed - b_signed;
+        result := (size downto 1 => '0') & not(result(size));
+      when others =>
+        result := not(a_signed or b_signed);
+    end case;
 
 end architecture alu_arch;
