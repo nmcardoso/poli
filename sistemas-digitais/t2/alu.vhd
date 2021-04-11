c5442a0c1c2e464583bd723d45116e5936224f0d
Natanael <natanael_magalhaes@msn.com>
2021-04-11 01:38:56 -0300

    alu arch

--- sistemas-digitais/t2/alu.vhd
+++ sistemas-digitais/t2/alu.vhd
@@ -29,5 +29,26 @@ architecture alu_arch of alu is
   signal SS: bit_vector(size - 1 downto 0) := (others => '0');
   -- signal signed_A, signed_B, signed_F: bit_vector(size downto 0) := (others => '0');
 begin
+  -- signed_A <= A(size - 1) & A;
+  -- signed_B <= B(size - 1) & B;
+  signed_a <= resize(signed(A), size + 1);
+  signed_b <= resize(signed(B), size + 1);
+  -- SS <= signed_F(size - 1 downto 0);
+  SS <= bit_vector(signed_F(size - 1 downto 0));
+  F <= signed_F(size) & SS(size-2 downto 0);
+
+  with S select
+    signed_F <= signed_A and signed_B when "0000",
+      signed_A or signed_B when "0001",
+      signed_A + signed_B when "0010",
+      signed_A - signed_B when "0110",
+      signed_A nor signed_B when others;
+
+  Co <= signed_F(size);
+
+  Ov <= '1' when 
+    ((S = "0010") and ((signed_A > 0 and signed_B > 0 and signed(SS) < 0) or (signed_A < 0 and signed_B < 0 and signed(SS) > 0))) or
+    ((S = "0110") and ((signed_A > 0 and signed_B < 0 and signed(SS) < 0) or (signed_A < 0 and signed_B > 0 and signed(SS) > 0)))
+    else '0';
 
 end architecture;
