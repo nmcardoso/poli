967b10ff9e147418d5767f814ae17127c86fb5fb
Natanael <natanael_magalhaes@msn.com>
2021-04-21 01:38:56 -0300

    init fom from file function

--- sistemas-digitais/t2/rom.vhd
+++ sistemas-digitais/t2/rom.vhd
@@ -13,3 +13,17 @@ entity rom is
   );
 end rom;
 
+architecture rom_arch of rom is
+  type RomType is array(0 to 255) of bit_vector(31 downto 0);
+
+  impure function init_rom_from_file(filename: in string) return RomType is
+    file rom_file: text is in filename;
+    variable file_line: line;
+    variable rom_data: RomType;
+  begin
+    for i in RomType'range loop
+      readline(rom_file, file_line);
+      read(file_line, rom_data(i));
+    end loop;
+    return rom_data;
+  end function;
