3d2018141516c84efcc1075dae57b2e9f635c2b6
Natanael <natanael_magalhaes@msn.com>
2021-06-23 02:02:02 -0300

    compute electrical field

--- eletromagnetismo/ec1/main.py
+++ eletromagnetismo/ec1/main.py
@@ -75,3 +75,47 @@ class FDM:
     # plt.plot(self.history)
     # plt.ylim(0, 6)
     # plt.show()
+
+
+  def compute_ef(self):
+    h = self.k * 1e-3
+    m = self.phi
+    if m is None:
+      raise 'call compute_potential() method first'
+
+    x, y = m.shape[1] - 1, m.shape[0] - 1
+    Ex = np.empty((y, x), dtype=float)
+    Ey = np.empty((y, x), dtype=float)
+    Ex[:] = np.nan
+    Ey[:] = np.nan
+
+    for j in range(y):
+      for i in range(x):
+        if np.isnan(m[j, i]):
+          continue
+
+        Ex[j, i] = (m[j, i] + m[j+1, i] - m[j, i+1] - m[j+1, i+1]) / (2 * h)
+        Ey[j, i] = (m[j, i] + m[j, i+1] - m[j+1, i] - m[j+1, i+1]) / (2 * h)
+
+    # plt.imshow(Ey)
+    # plt.show()
+
+    self.Ex = Ex
+    self.Ey = Ey
+
+    p = self.phi[~np.isnan(self.phi)]
+
+    lvl = np.linspace(np.amin(p), np.amax(p), int((np.amax(p) - np.amin(p)) / 10))
+    X, Y = np.meshgrid(np.linspace(0,x,x),np.linspace(0,y,y))
+    X1, Y1 = np.meshgrid(np.linspace(0,x + 1,x + 1),np.linspace(0,y +1,y+1))
+    # print(X.shape, Y.shape, Ex.shape, Ey.shape)
+    cs = plt.contour(X1, Y1, self.phi, colors='red', levels=lvl)
+    plt.clabel(cs, lvl, fontsize=8, inline_spacing=2, rightside_up=True, use_clabeltext=True)
+    # cb = plt.colorbar(cp)
+    # plt.show()
+    plt.streamplot(X, Y, Ex, Ey)
+    plt.gca().invert_yaxis()
+    plt.title(f'Iterações: {self.epochs}')
+    plt.show()
+
+
