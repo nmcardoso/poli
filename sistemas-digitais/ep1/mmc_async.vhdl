ceaa41e35d32a236b006e5598ce1b63c2c7ae95c
Natanael <natanael_magalhaes@msn.com>
2021-08-16 11:12:13 -0300

    entity

---
+++ sistemas-digitais/ep1/mmc_async.vhdl
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.numeric_bit.all;
+
+-- tentativa 01: 
+
+
+
+entity epmmc is
+  port(
+    reset, clock: in bit;
+    inicia: in bit;
+    A, B: in bit_vector(7 downto 0);
+    fim: out bit;
+    nSomas: out bit_vector(8 downto 0);
+    MMC: out bit_vector(15 downto 0)
+  );
+end epmmc;
