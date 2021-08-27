6b0f856b7dd50e3ac9640502b49da97648009cda
Natanael <natanael_magalhaes@msn.com>
2021-08-27 11:12:13 -0300

    accumulator A and B

--- sistemas-digitais/ep1/mmc_3.vhdl
+++ sistemas-digitais/ep1/mmc_3.vhdl
@@ -138,4 +138,7 @@ end process;
 add_a <= bit_vector(unsigned(bit_vector("00000000" & A)) + unsigned(a_reg));
 add_b <= bit_vector(unsigned(bit_vector("00000000" & B)) + unsigned(b_reg));
 acc_nSomas <= bit_vector(unsigned(acc_a_reg) + unsigned(acc_b_reg));
+-- acc_nSomas <= bit_vector(unsigned(nSomas_reg) + unsigned(acc_step));
+acc_a <= bit_vector(unsigned(acc_a_reg) + unsigned(acc_step));
+acc_b <= bit_vector(unsigned(acc_b_reg) + unsigned(acc_step));
 end architecture;
