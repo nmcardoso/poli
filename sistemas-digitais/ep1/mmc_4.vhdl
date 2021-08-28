a36ffc36df8faad91f4bf17eccc563e881234e9c
Natanael <natanael_magalhaes@msn.com>
2021-08-28 11:12:13 -0300

    fourth description of MMC hardware

---
+++ sistemas-digitais/ep1/mmc_4.vhdl
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.numeric_bit.all;
+
+
+
+entity mmc is
+  port(
+    reset, clock: in bit;
+    inicia: in bit;
+    A, B: in bit_vector(7 downto 0);
+    fim: out bit;
+    nSomas: out bit_vector(8 downto 0);
+    MMC: out bit_vector(15 downto 0)
+  );
+end mmc;
+
