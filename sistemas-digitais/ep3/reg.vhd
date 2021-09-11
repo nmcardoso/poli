3f98893bf409ff18e6990835df3095a5ac53fb09
Natanael <natanael_magalhaes@msn.com>
2021-09-11 11:12:13 -0300

    register

--- sistemas-digitais/ep3/reg.vhd
+++ sistemas-digitais/ep3/reg.vhd
@@ -13,3 +13,23 @@ entity reg is
     q: out bit_vector(wordSize-1 downto 0)
   );
 end reg;
+
+architecture reg_arch of reg is
+  signal data: bit_vector(wordSize-1 downto 0);
+begin
+
+  ffdr: process(clock, reset)
+  begin
+    if reset='1' then
+      data <= (others => '0');
+    elsif clock='1' and clock'event then
+      if load='1' then
+        data <= d;
+      end if;
+    end if;
+  end process;
+  q <= data;
+
+end architecture;
+
+
