3ed6a42c043b555c7055ad545bd8d74ca4051865
Natanael <natanael_magalhaes@msn.com>
2021-09-22 11:12:13 -0300

    register

--- sistemas-digitais/ep3/calc.vhd
+++ sistemas-digitais/ep3/calc.vhd
@@ -22,6 +22,43 @@ begin
     a_out(to_integer(unsigned(sel))) <= a_in;
   end process;
 end architecture;
+
+
+-- REGISTER
+
+library IEEE;
+use IEEE.numeric_bit.all;
+
+entity reg is
+  generic (
+    wordSize : natural := 4
+  );
+  port (
+    clock: in bit;
+    reset: in bit;
+    load: in bit;
+    d: in bit_vector(wordSize-1 downto 0);
+    q: out bit_vector(wordSize-1 downto 0)
+  );
+end reg;
+
+architecture reg_arch of reg is
+  signal data: bit_vector(wordSize-1 downto 0);
+begin
+  process(clock, reset)
+  begin
+    if reset = '1' then
+      data <= (others => '0');
+    elsif rising_edge(clock) then
+      if load = '1' then
+        data <= d;
+      end if;
+    end if;
+  end process;
+  q <= data;
+end reg_arch;
+
+
 -- REGFILE
 
 library IEEE;
