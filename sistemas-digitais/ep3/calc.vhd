39d32a60e28a1e06b5c20be1cc001601fe1a1aa9
Natanael <natanael_magalhaes@msn.com>
2021-09-25 11:12:13 -0300

    calculator

--- sistemas-digitais/ep3/calc.vhd
+++ sistemas-digitais/ep3/calc.vhd
@@ -205,3 +205,108 @@ begin
   sum <= result;
 end architecture;
 
+
+-- CALCULATOR
+
+library IEEE;
+use IEEE.numeric_bit.all;
+use IEEE.math_real.ceil;
+use IEEE.math_real.log2;
+
+entity calc is
+  port(
+    clock: in bit;
+    reset: in bit;
+    instruction: in bit_vector(16 downto 0);
+    q1: out bit_vector(15 downto 0)
+  );
+end entity;
+
+architecture calc_arch of calc is
+  component regfile is
+    generic (
+      regn: natural := 32;
+      wordSize: natural := 64
+    );
+    port (
+      clock: in bit;
+      reset: in bit;
+      regWrite: in bit;
+      rr1, rr2, wr: in bit_vector(natural(ceil(log2(real(regn))))-1 downto 0);
+      d: in bit_vector(wordSize-1 downto 0);
+      q1, q2: out bit_vector(wordSize-1 downto 0)
+    );
+  end component;
+
+  component fa is
+    generic(
+      size: natural := 16
+    );
+    port(
+      a, b: in bit_vector(size-1 downto 0);
+      c_in: in bit;
+      sum: out bit_vector(size-1 downto 0);
+      c_out: out bit;
+      ov: out bit
+    );
+  end component;
+
+    signal ov_flag : bit;
+    signal opcode: bit_vector(1 downto 0);
+    signal oper1, oper2, dest: bit_vector(4 downto 0);
+    signal result, oper1_reg, oper2_reg: bit_vector(15 downto 0);
+    signal imediate_or_reg2: bit_vector(15 downto 0);
+begin
+    reg_bank: regfile   
+      generic map(
+        regn => 32, 
+        wordSize => 16
+      )
+      port map(
+        clock => clock, 
+        reset => reset, 
+        regWrite => '1', 
+        rr1 => oper1, 
+        rr2 => oper2, 
+        wr => dest, 
+        d => result, 
+        q1 => oper1_reg, 
+        q2 => oper2_reg
+      );
+
+    -- adder: fa 
+    --   generic map(
+    --     size => 16
+    --   )
+    --   port map(
+    --     a => oper1_reg, 
+    --     b => imediate_or_reg2, 
+    --     c_in => '0', 
+    --     sum => result, 
+    --     c_out => open, 
+    --     ov => ov_flag
+    --   );
+
+    opcode <= instruction(16 downto 15);
+    oper2 <= instruction(14 downto 10);
+    oper1 <= instruction(9 downto 5);
+    dest <= instruction(4 downto 0);
+
+    with opcode select result <= 
+      -- bit_vector(to_signed(to_integer(signed(oper1_reg), 16) + to_integer(signed(oper2_reg), 16), 16)) when "00",
+      bit_vector(resize(signed(oper1_reg), 16) + resize(signed(oper2_reg), 16)) when "00",
+      bit_vector(resize(signed(oper1_reg), 16) + resize(signed(oper2), 16)) when "01",
+      bit_vector(resize(signed(oper1_reg), 16) - resize(signed(oper2_reg), 16)) when "10",
+      bit_vector(resize(signed(oper1_reg), 16) - resize(signed(oper2), 16)) when "11";
+
+    -- imediate_or_reg2 <=
+    --   bit_vector(to_unsigned(to_integer(unsigned(oper2)), 16)) when (opcode = "00" and oper2(4) = '0') or (opcode = "11" and oper2(4) = '1') else
+    --   bit_vector(resize(signed(oper2), 16)) when (opcode = "01" and oper2(4) = '0') or (opcode = "")
+
+    -- with opcode select imediate_or_reg2 <=  
+    --   bit_vector(to_unsigned(to_integer(unsigned(oper2)), 16)) when "00",
+    --   bit_vector(resize(signed(oper2), 16)) when "01",
+    --   bit_vector(-signed(resize(signed(oper2), 16))) when others;
+
+    q1 <= oper1_reg;
+end calc_arch;
