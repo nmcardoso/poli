70bca301e752a80b1c96f0c5e0d804a2122085df
Natanael <natanael_magalhaes@msn.com>
2021-10-20 20:28:59 -0300

    atualizando gráficos e modelo

--- lab-controle/exp7/exp7.m
+++ lab-controle/exp7/exp7.m
@@ -9,17 +9,17 @@ Kp = 1.59; % Kp (exp. 2)
 KNL = 59.85; % K não-linear (exp. 4)
 phi = 1.25; % phi (exp. 4)
 n = 1 / 3;
-phi = 1.9; % atrito de Coulomb
+phi = 1.2; % atrito de Coulomb
 tr = [0.3 0.5 1]; % tempos de subida (exp. 7)
 Kcs = [0.5 1 1.5]; % Kcs (exp. 7)
-KpNL = 0;
-KpL = 0;
 Kc = Kcs(2);
 KKt = K*Kt;
 KtL = KKt / K; % Kt linear
 KtNL = KKt / KNL; % Kt não-linear
-Kpi = 0.661 / tr(1);
+Kpi = 0.661 / tr(3);
+% Kpi = 0.24;
 U = Kp * pi / 2; % amplitude do degrau, equivalente a 90deg em volts
+Ti = T;
 
 item_e = false;
 item_f = true;
@@ -63,8 +63,31 @@ if item_f
   sim('modelo', 10);
   figure;
   hold on;
-  plot(VmNL)
-  plot(VmL)
+  plot(tsim, U_sim)
+  plot(tsim, VmNL)
+  plot(tsim, VmL)
+  legend('Entrada', 'V_m não linear', 'V_m linear');
+  grid on;
+  hold off;
+
+
+  figure;
+  hold on;
+  colors = {
+    [0.8500 0.3250 0.0980], 
+    [0.4660 0.6740 0.1880], 
+    [0.4940 0.1840 0.5560]
+  };
+  plot(tsim, U_sim)
+  for i = 1:3
+    Kpi = 0.661 / tr(i);
+    sim('modelo', 10);
+    plot(tsim, Vt_simNL, '--', 'Color', colors{i}, 'LineWidth', 1.1);
+    plot(tsim, Vt_simL, '-', 'Color', colors{i}, 'LineWidth', 1.1);
+  end;
+  ylim([0, U + 0.05]);
+  legend('Entrada', 'V_t não linear (t_r = 0.3)', 'V_t linear (t_r = 0.3)', 'V_t não linear (t_r = 0.5)', 'V_t linear (t_r = 0.5)', 'V_t não linear (t_r = 1.0)', 'V_t linear (t_r = 1.0)');
+  grid on;
   hold off;
   % opt = stepDataOptions('StepAmplitude', U);
   % Wn = sqrt((Kc * K * Kp * n^2) / T);
