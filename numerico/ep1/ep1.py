f3e87b5515b5ef1af6952b8f4b9963696893654e
Natanael <natanael_magalhaes@msn.com>
2022-04-22 11:11:11 -0300

    algoritmo de decomposição lu

--- numerico/ep1/ep1.py
+++ numerico/ep1/ep1.py
@@ -16,3 +16,19 @@ def get_values(
     a[0] = 0
     c[-1] = 0
   return a, b, c, d
+
+
+def decomp_lu(
+  a: np.ndarray, 
+  b: np.ndarray, 
+  c: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray]:
+  n = len(a)
+  u = np.zeros(shape=(n,))
+  l = np.zeros(shape=(n,))
+
+  u[0] = b[0]
+  for i in range(1, n):
+    l[i] = a[i] / u[i-1]
+    u[i] = b[i] - l[i] * c[i-1]
+  return l, u
