b2575aa677e625e70fde2f877f629293bbe1999c
Natanael <natanael_magalhaes@msn.com>
2022-04-22 11:11:11 -0300

    gera valores de teste

---
+++ numerico/ep1/ep1.py
@@ -0,0 +1,18 @@
+from typing import Tuple
+import numpy as np
+import argparse
+
+
+def get_values(
+  n: int, 
+  cyclic: bool = False
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+  a = np.array([(2*i - 1) / (4*i) for i in range(1, n)] + [(2*n - 1)/(2*n)])
+  b = np.array([2]*n)
+  c = 1. - a
+  d = np.array([np.cos((2*np.pi*i**2)/(n**2)) for i in range(1, n+1)])
+
+  if not cyclic:
+    a[0] = 0
+    c[-1] = 0
+  return a, b, c, d
