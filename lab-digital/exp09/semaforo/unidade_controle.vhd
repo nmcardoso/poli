acdde98431a7e79306a314758e38a4ef1f9e8116
Natanael <natanael_magalhaes@msn.com>
2022-06-10 10:11:12 -0300

    atualização da unidade de controle para o novo fluxo de dados

--- lab-digital/exp09/semaforo/unidade_controle.vhd
+++ lab-digital/exp09/semaforo/unidade_controle.vhd
@@ -7,11 +7,11 @@ entity unidade_controle is
   port (
     clk, reset_n: in std_logic;
     T0, T1, T2: in std_logic_vector(3 downto 0);
-    Qa, Qb, Qc, Qd: in std_logic;
+    end_n: in std_logic;
     load_n: out std_logic;
     S0_verde, S0_amarelo, S0_vermelho: out std_logic;
     S1_verde, S1_amarelo, S1_vermelho: out std_logic;
-    Oa, Ob, Oc, Od: out std_logic
+    offset: out std_logic_vector(3 downto 0)
   );
 end unidade_controle;
 
@@ -19,8 +19,6 @@ architecture unidade_controle_arch of unidade_controle is
   type estado_tipo is (IDLE, E0, E1, E2, E3, E4, E5);
   signal estado_atual, prox_estado: estado_tipo;
   signal luzes: std_logic_vector(5 downto 0);
-  signal contagem: std_logic_vector(3 downto 0);
-  signal offset: std_logic_vector(3 downto 0);
   constant max: unsigned := "1111";
   constant one: unsigned := "0001";
 begin
@@ -33,7 +31,7 @@ begin
     end if;
   end process;
 
-  circ_prox_estado: process(estado_atual, contagem)
+  circ_prox_estado: process(estado_atual, end_n)
   begin
     case estado_atual is
       when IDLE =>
@@ -44,7 +42,7 @@ begin
       when E0 =>
         offset <= std_logic_vector(max - unsigned(T1) + one);
         luzes <= "100001";
-        if contagem /= "1111" then
+        if end_n = '1' then
           prox_estado <= E0;
           load_n <= '1';
         else
@@ -54,7 +52,7 @@ begin
       when E1 =>
         offset <= std_logic_vector(max - unsigned(T2) + one);
         luzes <= "010001";
-        if contagem /= "1111" then
+        if end_n = '1' then
           prox_estado <= E1;
           load_n <= '1';
         else
@@ -64,7 +62,7 @@ begin
       when E2 =>
         offset <= std_logic_vector(max - unsigned(T0) + one);
         luzes <= "001001";
-        if contagem /= "1111" then
+        if end_n = '1' then
           prox_estado <= E2;
           load_n <= '1';
         else
@@ -74,7 +72,7 @@ begin
       when E3 =>
         offset <= std_logic_vector(max - unsigned(T1) + one);
         luzes <= "001100";
-        if contagem /= "1111" then
+        if end_n = '1' then
           prox_estado <= E3;
           load_n <= '1';
         else
@@ -84,7 +82,7 @@ begin
       when E4 =>
         offset <= std_logic_vector(max - unsigned(T2) + one);
         luzes <= "001010";
-        if contagem /= "1111" then
+        if end_n = '1' then
           prox_estado <= E4;
           load_n <= '1';
         else
@@ -94,7 +92,7 @@ begin
       when E5 =>
         offset <= std_logic_vector(max - unsigned(T0) + one);
         luzes <= "001001";
-        if contagem /= "1111" then
+        if end_n = '1' then
           prox_estado <= E5;
           load_n <= '1';
         else
@@ -104,17 +102,10 @@ begin
     end case;
   end process;
 
-  contagem <= Qd & Qc & Qb & Qa;
-
   S0_verde <= luzes(5);
   S0_amarelo <= luzes(4);
   S0_vermelho <= luzes(3);
   S1_verde <= luzes(2);
   S1_amarelo <= luzes(1);
   S1_vermelho <= luzes(0);
-
-  Od <= offset(3);
-  Oc <= offset(2);
-  Ob <= offset(1);
-  Oa <= offset(0);
 end architecture;
