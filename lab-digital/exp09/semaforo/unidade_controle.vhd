d8423ff9502df83bfe82c2ff6a3daa363d3b5113
Natanael <natanael_magalhaes@msn.com>
2022-06-27 21:43:48 -0300

    correção do fluxo de dados

--- lab-digital/exp09/semaforo/unidade_controle.vhd
+++ lab-digital/exp09/semaforo/unidade_controle.vhd
@@ -6,101 +6,189 @@ use IEEE.numeric_std.all;
 entity unidade_controle is
   port (
     clk, reset_n: in std_logic;
-    T0, T1, T2: in std_logic_vector(3 downto 0);
-    end_n: in std_logic;
-    load_n: out std_logic;
+    count: in std_logic_vector(3 downto 0);
+    -- count_min: in std_logic;
+    load_n: out std_logic; -- sinal de controle do contador
+    Ts: out std_logic_vector(1 downto 0); --sinal de controle do mux
     S0_verde, S0_amarelo, S0_vermelho: out std_logic;
-    S1_verde, S1_amarelo, S1_vermelho: out std_logic;
-    offset: out std_logic_vector(3 downto 0)
+    S1_verde, S1_amarelo, S1_vermelho: out std_logic
   );
 end unidade_controle;
 
 architecture unidade_controle_arch of unidade_controle is
-  type estado_tipo is (IDLE, E0, E1, E2, E3, E4, E5);
+  type estado_tipo is (E0L, E0C, E1L, E1C, E2L, E2C, E3L, E3C, E4L, E4C, E5L, E5C);
   signal estado_atual, prox_estado: estado_tipo;
   signal luzes: std_logic_vector(5 downto 0);
-  constant max: unsigned := "1111";
-  constant one: unsigned := "0001";
 begin
   sincrono: process(clk, reset_n, prox_estado)
   begin
     if reset_n='0' then
-      estado_atual <= IDLE;
+      estado_atual <= E0L;
     elsif clk'event and clk='1' then
       estado_atual <= prox_estado;
     end if;
   end process;
 
-  circ_prox_estado: process(estado_atual, end_n)
+  circ_prox_estado: process(estado_atual, count)
   begin
     case estado_atual is
-      when IDLE =>
-        offset <= std_logic_vector(max - unsigned(T0) + one);
-        prox_estado <= E0;
-        luzes <= "000000";
-        load_n <= '0';
-      when E0 =>
-        offset <= std_logic_vector(max - unsigned(T1) + one);
+      when E0L =>
+        prox_estado <= E0C;
+        Ts <= "00";
         luzes <= "100001";
-        if end_n = '1' then
-          prox_estado <= E0;
-          load_n <= '1';
+        load_n <= '0';
+      when E0C =>
+        Ts <= "00";
+        luzes <= "100001";
+        load_n <= '1';
+        if count = "0001" then
+          prox_estado <= E1L;
         else
-          prox_estado <= E1;
-          load_n <= '0';
+          prox_estado <= E0C;
         end if;
-      when E1 =>
-        offset <= std_logic_vector(max - unsigned(T2) + one);
+      when E1L =>
+        prox_estado <= E1C;
+        Ts <= "10";
         luzes <= "010001";
-        if end_n = '1' then
-          prox_estado <= E1;
-          load_n <= '1';
+        load_n <= '0';
+      when E1C =>
+        Ts <= "10";
+        luzes <= "010001";
+        load_n <= '1';
+        if count = "0001" then
+          prox_estado <= E2L;
         else
-          prox_estado <= E2;
-          load_n <= '0';
+          prox_estado <= E1C;
         end if;
-      when E2 =>
-        offset <= std_logic_vector(max - unsigned(T0) + one);
+      when E2L =>
+        prox_estado <= E2C;
+        Ts <= "00";
         luzes <= "001001";
-        if end_n = '1' then
-          prox_estado <= E2;
-          load_n <= '1';
+        load_n <= '0';
+      when E2C =>
+        Ts <= "00";
+        luzes <= "001001";
+        load_n <= '1';
+        if count = "0001" then
+          prox_estado <= E3L;
         else
-          prox_estado <= E3;
-          load_n <= '0';
+          prox_estado <= E2C;
         end if;
-      when E3 =>
-        offset <= std_logic_vector(max - unsigned(T1) + one);
+      when E3L =>
+        prox_estado <= E3C;
+        Ts <= "01";
         luzes <= "001100";
-        if end_n = '1' then
-          prox_estado <= E3;
-          load_n <= '1';
+        load_n <= '0';
+      when E3C =>
+        Ts <= "01";
+        luzes <= "001100";
+        load_n <= '1';
+        if count = "0001" then
+          prox_estado <= E4L;
         else
-          prox_estado <= E4;
-          load_n <= '0';
+          prox_estado <= E3C;
         end if;
-      when E4 =>
-        offset <= std_logic_vector(max - unsigned(T2) + one);
+      when E4L =>
+        prox_estado <= E4C;
+        Ts <= "10";
         luzes <= "001010";
-        if end_n = '1' then
-          prox_estado <= E4;
-          load_n <= '1';
+        load_n <= '0';
+      when E4C =>
+        Ts <= "10";
+        luzes <= "001010";
+        load_n <= '1';
+        if count = "0001" then
+          prox_estado <= E5L;
         else
-          prox_estado <= E5;
-          load_n <= '0';
+          prox_estado <= E4C;
         end if;
-      when E5 =>
-        offset <= std_logic_vector(max - unsigned(T0) + one);
+      when E5L =>
+        prox_estado <= E5C;
+        Ts <= "00";
         luzes <= "001001";
-        if end_n = '1' then
-          prox_estado <= E5;
-          load_n <= '1';
+        load_n <= '0';
+      when E5C =>
+        Ts <= "00";
+        luzes <= "001001";
+        load_n <= '1';
+        if count = "0001" then
+          prox_estado <= E0L;
         else
-          prox_estado <= E0;
-          load_n <= '0';
+          prox_estado <= E5C;
         end if;
     end case;
   end process;
+
+  -- circ_prox_estado: process(estado_atual, count_min)
+  -- begin
+  --   case estado_atual is
+  --     when IDLE =>
+  --       prox_estado <= E0;
+  --       Ts <= "00";
+  --       luzes <= "100001";
+  --       load_n <= '0';
+  --     when E0 =>
+  --       Ts <= "01";
+  --       luzes <= "100001";
+  --       if count = "0001" then
+  --         prox_estado <= E0;
+  --         load_n <= '1';
+  --       else
+  --         prox_estado <= E1;
+  --         load_n <= '0';
+  --       end if;
+  --     when E1 =>
+  --       Ts <= "10";
+  --       luzes <= "010001";
+  --       if count = "0001" then
+  --         prox_estado <= E1;
+  --         load_n <= '1';
+  --       else
+  --         prox_estado <= E2;
+  --         load_n <= '0';
+  --       end if;
+  --     when E2 =>
+  --       Ts <= "00";
+  --       luzes <= "001001";
+  --       if count = "0001" then
+  --         prox_estado <= E2;
+  --         load_n <= '1';
+  --       else
+  --         prox_estado <= E3;
+  --         load_n <= '0';
+  --       end if;
+  --     when E3 =>
+  --       Ts <= "01";
+  --       luzes <= "001100";
+  --       if count = "0001" then
+  --         prox_estado <= E3;
+  --         load_n <= '1';
+  --       else
+  --         prox_estado <= E4;
+  --         load_n <= '0';
+  --       end if;
+  --     when E4 =>
+  --       Ts <= "10";
+  --       luzes <= "001010";
+  --       if count = "0001" then
+  --         prox_estado <= E4;
+  --         load_n <= '1';
+  --       else
+  --         prox_estado <= E5;
+  --         load_n <= '0';
+  --       end if;
+  --     when E5 =>
+  --       Ts <= "00";
+  --       luzes <= "001001";
+  --       if count = "0001" then
+  --         prox_estado <= E5;
+  --         load_n <= '1';
+  --       else
+  --         prox_estado <= E0;
+  --         load_n <= '0';
+  --       end if;
+  --   end case;
+  -- end process;
 
   S0_verde <= luzes(5);
   S0_amarelo <= luzes(4);
