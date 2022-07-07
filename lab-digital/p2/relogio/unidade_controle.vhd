75cd92f75fb462203cd2602be7a2a034ff6c3039
Natanael <natanael_magalhaes@msn.com>
2022-07-07 09:19:02 -0300

    estado idle

--- lab-digital/p2/relogio/unidade_controle.vhd
+++ lab-digital/p2/relogio/unidade_controle.vhd
@@ -12,13 +12,13 @@ end unidade_controle;
 
 
 architecture unidade_controle_arch of unidade_controle is
-  type estado_tipo is (PROG, OP);
+  type estado_tipo is (PROG, OP, IDLE);
   signal estado_atual, prox_estado: estado_tipo;
 begin
   sincrono: process(clk, clear, prox_estado)
   begin
     if clear='1' then
-      estado_atual <= PROG;
+      estado_atual <= IDLE;
     elsif clk'event and clk='1' then
       estado_atual <= prox_estado;
     end if;
@@ -29,26 +29,52 @@ begin
   --   case estado_atual is
   --     when PROG =>
   --       c_inibe_n <= '0';
-  --       c_carrega_n <= not grava;
+  --       -- c_carrega_n <= not grava;
+        
+  --       if grava = '1' then
+  --         c_carrega_n <= '0';
+  --       else
+  --         c_carrega_n <= '1';
+  --       end if;
+
   --       if programa = '1' then
   --         prox_estado <= PROG;
   --       elsif programa = '0' then
   --         prox_estado <= OP;
   --       end if;
   --     when OP =>
-  --       c_inibe_n <= not inibe;
-  --       c_carrega_n <= '1';
+  --       -- c_inibe_n <= not inibe;
+  --       c_carrega_n <= '0';
+
+  --       if inibe = '1' then
+  --         c_inibe_n <= '0';
+  --       else
+  --         c_inibe_n <= '1';
+  --       end if;
+
   --       if programa = '1' then
   --         prox_estado <= PROG;
   --       elsif programa = '0' then
   --         prox_estado <= OP;
   --       end if;
+  --     when IDLE =>
+  --       c_inibe_n <= '0';
+  --       c_carrega_n <= '1';
+  --       if programa = '1' then
+  --         prox_estado <= PROG;
+  --       else
+  --         prox_estado <= IDLE;
+  --       end if;
   --   end case;
   -- end process;
 
-  prox_estado <= PROG when programa = '1' else OP;
+  
+  prox_estado <= 
+    IDLE when estado_atual = IDLE and programa = '0' else
+    PROG when programa = '1' else 
+    OP;
 
-  c_inibe_n <= '0' when estado_atual = PROG else not inibe;
+  c_inibe_n <= not inibe when estado_atual = OP else '0';
 
   c_carrega_n <= not grava when estado_atual = PROG else '1';
 
