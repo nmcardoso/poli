4c55fcf192ba143a6b449d947156a6209dfdba7f
Natanael <natanael_magalhaes@msn.com>
2022-07-07 10:13:01 -0300

    fixpath

--- lab-digital/p2/fixpath/fixpath.py
+++ lab-digital/p2/fixpath/fixpath.py
@@ -1,18 +1,35 @@
 from pathlib import Path
 import os
+import re
 
+PATH_TO_REPLACE = '/home/natan/repos/poli/lab-digital/p2/relogio'
+PROJECT_PATH = Path(__file__).parent
+PATTERNS = [
+  '--vector_source=\"(.*?)\"',
+  '--testbench_file=\"(.*?)\"',
+  '--output_directory=\"(.*?)\"'
+]
+
+flatten = lambda xss: [x for xs in xss for x in xs]
 
 def main():
-  project_path = Path(__file__).parent
-
-  for file in project_path.glob('*.vwf'):
+  for file in PROJECT_PATH.glob('*.vwf'):
     with file.open('r') as fp:
       file_content = fp.read()
     
-    new_content = file_content.replace(
-      '/home/natan/repos/poli/lab-digital/p2/relogio/', 
-      str(project_path.absolute()) + os.sep
-    )
+    matches = flatten([re.findall(p, file_content) for p in PATTERNS])
+
+    targets = [
+      m.replace(
+        PATH_TO_REPLACE, 
+        str(PROJECT_PATH.absolute())
+      ).replace('/', os.sep) 
+      for m in matches
+    ]
+
+    new_content = file_content
+    for source, target in zip(matches, targets):
+      new_content = new_content.replace(source, target) 
 
     with file.open('w') as fp:
       fp.write(new_content)
