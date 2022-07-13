c21f0f5cfade9ae68b6f23afe65bb623e2d2e59b
Natanael <natanael_magalhaes@msn.com>
2022-07-13 02:15:46 -0300

    pin planner

--- lab-digital/p2/relogio/unidade_controle.vhd
+++ lab-digital/p2/relogio/unidade_controle.vhd
@@ -2,14 +2,12 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-
 entity unidade_controle is
   port (
     clk, clear, programa, grava, inibe: in std_logic;
     c_carrega_n, c_inibe_n: out std_logic
   );
 end unidade_controle;
-
 
 architecture unidade_controle_arch of unidade_controle is
   type estado_tipo is (PROG, OP, IDLE);
@@ -28,9 +26,6 @@ begin
     IDLE when estado_atual = IDLE and programa = '0' else
     PROG when programa = '1' else 
     OP;
-
   c_inibe_n <= not inibe when estado_atual = OP else '0';
-
   c_carrega_n <= not grava when estado_atual = PROG else '1';
-
 end architecture;
